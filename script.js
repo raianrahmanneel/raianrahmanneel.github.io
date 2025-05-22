82db46bd42c9f372d6da1a4a372386221f21c07d
raianrahmanneel <raianrahman112299@gmail.com>
2025-05-22 06:42:08 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+document.querySelector('.name').addEventListener('mouseover', () => {
+  document.querySelector('.name').style.color = '#00FFFF';
+});
+document.querySelector('.name').addEventListener('mouseout', () => {
+  document.querySelector('.name').style.color = '#FFD700';
+});
